42b4bce3db170012eccd7c02b38975c5f8e5f0de
stefankoppier <stefan.koppier@outlook.com>
2023-04-21 12:39:25 +0200

    Added renovate config

---
+++ .github/config/renovate.js
@@ -0,0 +1,10 @@
+module.exports = {
+    branchPrefix: 'renovate',
+    username: 'renovate-release',
+    gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+    onboarding: false,
+    includeForks: false
+    platform: 'github'
+    dryRun: 'full',
+    repositories: ['stefankoppier/oasdiff-gradle']
+}
